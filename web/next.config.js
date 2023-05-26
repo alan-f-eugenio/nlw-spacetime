e7c361c66f8f2cfd38669eb9ca94b08addb469d7
Alan Eugenio <alan.349@hotmail.com>
2023-05-26 14:01:20 -0300

    Full Application (Server, Web and Mobile)

---
+++ web/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['avatars.githubusercontent.com', '192.168.1.11', 'placehold.co'],
+  },
+}
+
+module.exports = nextConfig
